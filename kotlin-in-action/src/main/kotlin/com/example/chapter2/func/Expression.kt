da724f10aa398dd0d5fb09efb38b70d8cf07becc
seokrae <kslbsh@gmail.com>
2022-06-07 23:51:33 +0900

    feat: 함수와 변수

---
+++ kotlin-in-action/src/main/kotlin/com/example/chapter2/func/Expression.kt
@@ -0,0 +1,5 @@
+package com.example.chapter2.func
+
+fun max(a: Int, b: Int) : Int {
+    return if (a > b) a else b
+}
---
+++ kotlin-in-action/src/main/kotlin/com/example/chapter2/func/HelloWorld.kt
@@ -0,0 +1,5 @@
+package com.example.chapter2.func
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
