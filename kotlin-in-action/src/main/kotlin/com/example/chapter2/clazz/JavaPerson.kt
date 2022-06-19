dc018a8b98e13f0886d9f8381a286b9bb23c0dce
seokrae <kslbsh@gmail.com>
2022-06-19 18:53:26 +0900

    feat: 커스텀 접근자

--- kotlin-in-action/src/main/kotlin/com/example/chapter2/clazz/JavaPerson.kt
+++ kotlin-in-action/src/main/kotlin/com/example/chapter2/clazz/JavaPerson.kt
@@ -1,3 +1,3 @@
 package com.example.chapter2.clazz
 
-class JavaPerson(private val name: String)
+class JavaPerson(val name: String)
---
+++ kotlin-in-action/src/main/kotlin/com/example/chapter2/getters/2.2.3_1_Rectangle.kt
@@ -0,0 +1,20 @@
+package com.example.chapter2.getters
+
+import java.util.*
+
+class Rectangle(val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
+}
+
+fun createRandomRectangle(): Rectangle {
+    val random = Random()
+    return Rectangle(random.nextInt(), random.nextInt())
+}
+
+fun main() {
+    println("createRandomRectangle() = ${createRandomRectangle().isSquare}")
+    println("Rectangle().isSquare = ${Rectangle(5, 5).isSquare}")
+}
