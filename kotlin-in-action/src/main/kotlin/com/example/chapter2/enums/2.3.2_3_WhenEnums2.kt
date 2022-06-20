f1796094875327750b8f985d70ecf8912c0b33fb
seokrae <kslbsh@gmail.com>
2022-06-20 22:54:30 +0900

    feat: enum & when의 활용

--- kotlin-in-action/src/main/kotlin/com/example/chapter2/enums/2.3.2_3_WhenEnums2.kt
+++ kotlin-in-action/src/main/kotlin/com/example/chapter2/enums/2.3.2_3_WhenEnums2.kt
@@ -1,9 +1,11 @@
 package com.example.chapter2.enums
 
+// 짧은 이름으로 사용하기 위해 enum 상수를 모두 임포트
 import com.example.chapter2.enums.Color1.*
 
 
 fun getWarmth(color: Color1) = when(color) {
+    // 임포트한 enum 상수를 이름만으로 사용
     RED, ORANGE, YELLOW -> "warm"
     GREEN -> "neutral"
     BLUE, INDIGO, VIOLET -> "cold"
