6e0572915b6c186b35f8c78207f86837e2e55193
seokrae <kslbsh@gmail.com>
2022-06-24 23:48:00 +0900

    feat: 스마트 캐스트 테스트

---
+++ kotlin-in-action/src/main/kotlin/com/example/chapter2/smartcast/2.3.5_SmartCasts.kt
@@ -0,0 +1,21 @@
+package com.example.chapter2.smartcast
+
+// 인터페이스 선언 -> 숫자 또는 식
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        val n = e as Num
+        return n.value
+    }
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
+
+fun main() {
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+}
