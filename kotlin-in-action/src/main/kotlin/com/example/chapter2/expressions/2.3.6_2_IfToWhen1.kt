cb509df96ee324d6bcc6525847e6507818fe2b86
seokrae <kslbsh@gmail.com>
2022-06-24 23:55:26 +0900

    feat: expression (if/when)

---
+++ kotlin-in-action/src/main/kotlin/com/example/chapter2/expressions/2.3.6_2_IfToWhen1.kt
@@ -0,0 +1,15 @@
+package com.example.chapter2.expressions
+
+fun eval2(e: Expr): Int =
+    when (e) {
+        is Num ->
+            e.value
+        is Sum ->
+            eval(e.right) + eval(e.left)
+        else ->
+            throw IllegalArgumentException("Unknown expression")
+    }
+
+fun main(args: Array<String>) {
+    println(eval(Sum(Num(1), Num(2))))
+}
